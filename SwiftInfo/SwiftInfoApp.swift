ac403efde8f6070cb8b372729cbd24b51b1a973e
mrzogy <magbolmrzogy@gmail.com>
2023-05-16 10:24:14 +0300

    Initial Commit

---
+++ SwiftInfo/SwiftInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftInfoApp.swift
+//  SwiftInfo
+//
+//  Created by 3bood on 26/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
